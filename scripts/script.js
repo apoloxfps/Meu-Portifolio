d8d2a88e0a68956f5fa710d1ca3b3b92d94f3f0a
Nicolas Andrade <nicleicaio2@gmail.com>
2025-10-27 03:13:07 -0300

    projeto em processo de finalização

---
+++ scripts/script.js
@@ -0,0 +1,45 @@
+
+const botao = document.getElementById('botao-tema');
+const body  = document.body;
+
+//Persistência do tema
+const temasalvo = localStorage.getItem('tema');
+temaEscuro(temasalvo === 'escuro');
+
+//Funçao para alternar entre tema claro e escuro
+function temaEscuro(tipo) {
+    if (tipo == true) {
+        body.classList.add('escuro');
+        botao.innerHTML = '<i class="fa-solid fa-sun"></i>';
+    }
+    else {
+        body.classList.remove('escuro');
+        botao.innerHTML = '<i class="fa-solid fa-moon"></i>';
+    }
+}
+
+botao.addEventListener('click', () => {
+    const isescuro = body.classList.toggle('escuro');
+    temaEscuro(isescuro);
+    localStorage.setItem('tema', isescuro ? 'escuro' : 'claro');
+});
+
+//scroll suave para links de navegação
+const navLinks = document.querySelectorAll('#menu ul a.link');
+navLinks.forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute
+            ('href'));
+            if (target) {
+                const headerHeight = document.querySelector('header').
+                offsetHeight;
+                const targetPosition = target.getBoundingClientRect().top + window.scrollY - headerHeight - 20;
+                console.log('Scroll para:', targetPosition);
+                window.scrollTo({
+                    top: targetPosition, behavior: 'smooth'
+                    
+                });
+            };
+    });
+});
